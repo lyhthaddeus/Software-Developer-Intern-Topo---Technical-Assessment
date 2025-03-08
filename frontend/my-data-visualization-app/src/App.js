ad2a25fcf25861c308d5e835d04ec15b2104c88d
lyhthaddeus <lyhthaddeus@gmail.com>
2025-03-09 06:22:32 +0800

    Add frontend skeleton

---
+++ frontend/my-data-visualization-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import ChartComponent from './ChartComponent'; // Assuming the ChartComponent file is named ChartComponent.js
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ChartComponent /> */}
+    </div>
+  );
+}
+
+export default App;
